3d098bfd2066ffbe26c28ad7465695ffa27098ff
priyanshu2701 <priyanshuofficial2709@gmail.com>
2026-01-09 11:51:08 +0530

    1st commit this morning

--- app.py
+++ app.py
@@ -43,7 +43,8 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
-app.config['MYSQL_DB'] = os.getenv('MYSQL_DB') 
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT'))  # 🚨 REQUIRED
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor' # Return rows as dictionaries # Return rows as dictionaries
 mysql = MySQL(app)
 
